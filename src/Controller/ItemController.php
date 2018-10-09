75b6c080cb7ffa6f88db97d79bc5cfcd3315e041
Jovanela Daribo <carly972@live.fr>
2018-10-10 00:15:42 +0200

    Quête MVC 2

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -1,8 +1,16 @@
 <?php
-// src/Controller/ItemController.php
-require __DIR__ . '/../Model/ItemManager.php';
 
-$items = selectAllItems();
+namespace Controller;
+use Model\ItemManager;
 
-require __DIR__ . '/../View/item.php';
+
+class ItemController
+{
+    public function index()
+    {
+        $itemManager = new ItemManager();
+        $items = $itemManager->selectAllItems();
+        require __DIR__ . '/../View/item.php';
+    }
+}
 ?>
--- src/View/item.php
+++ src/View/item.php
@@ -1,17 +1,11 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Simple MVC</title>
-
-    <link rel="stylesheet" href="/assets/css/styles.css">
-    <link rel="icon" href="/assets/images/favicon.png">
-</head>
+<?php
+namespace View;
+?>
 
 
-<!-- affichage des items récupérés par la requête SQL -->
+<!DOCTYPE html>
+<html>
+<head> MVC </head>
 <body>
 <section>
     <h1>Items</h1>
